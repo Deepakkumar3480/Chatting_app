5962920cef1fc86678a7694be8da311af4b762a7
Deepak Kumar <71894229+Deepakkumar3480@users.noreply.github.com>
2025-06-26 21:22:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -55,13 +55,13 @@ app.use("/api/messages", messageRouter);
 await conncectDB();
 
 //it only run for the localhost
-if (process.env.NODE_ENV !== "production") {
+// if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
 
   server.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
   });
-}
+// }
 
 //export server for vercel
-export default server;
+// export default server;
